51b545e28ff43679ab9e2c8221c00dbdb0a7e52c
dev-junehee <dev.junehee@gmail.com>
2024-07-02 17:06:24 +0900

    Feat: 메인 스토리보드 삭제, 리유저블 프로토콜 구현, 전체목록 화면 엔트리포인트 설정

---
+++ haeyazi/Base/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  haeyazi
+//
+//  Created by junehee on 7/2/24.
+//
+
+import Foundation
---
+++ haeyazi/Base/BaseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BaseViewController.swift
+//  haeyazi
+//
+//  Created by junehee on 7/2/24.
+//
+
+import Foundation
