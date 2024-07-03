79006104119d87bc984fa45b4229c4b36b6e0430
dev-junehee <dev.junehee@gmail.com>
2024-07-03 15:27:27 +0900

    Design: NavigationController BarButton TintColor 수정 (primary)

---
+++ haeyazi/EndDate/EndDateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndDateViewController.swift
+//  haeyazi
+//
+//  Created by junehee on 7/3/24.
+//
+
+import UIKit
+
+class EndDateViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("마감일 화면 진입")
+    }
+    
+}
---
+++ haeyazi/Priority/PriorityViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PriorityViewController.swift
+//  haeyazi
+//
+//  Created by junehee on 7/3/24.
+//
+
+import Foundation
+
+class PriorityViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("우선순위 화면 진입")
+    }
+    
+}
